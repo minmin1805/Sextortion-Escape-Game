7f975884b64bc55b135db2588b0b97018ff044e0
Minh Doan <doanlyminh2005@gmail.com>
2026-02-17 23:19:18 -0500

    finished the design for frame1, frame2 page

--- frontend/pages/Frame1.jsx
+++ frontend/pages/Frame1.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import backgroundImage from "../src/assets/background/cosmicbackground.png";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
 
 function Frame1() {
+  const navigate = useNavigate();
+
+  const handleContinue = () => {
+    navigate("/frame2");
+  };
   return (
-    <div>
-      <h1>Frame 1</h1>
+    <div
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col items-center justify-between py-10"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="flex-1 flex items-center justify-center px-4">
+        <h1 className="text-white text-4xl font-bold text-center">
+          Every day, hundreds of teens receive a threatening message...
+        </h1>
+      </div>
+      <button onClick={handleContinue} className="bg-[#8B5CF6] text-white px-4 py-2 rounded-md absolute bottom-10 right-10 flex items-center gap-2">
+        Continue <FaLongArrowAltRight />
+      </button>
     </div>
-  )
+  );
 }
 
-export default Frame1
+export default Frame1;
--- frontend/pages/Frame2.jsx
+++ frontend/pages/Frame2.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import backgroundImage from "../src/assets/background/cosmicbackground.png";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 function Frame2() {
   return (
-    <div>
-      <h1>Frame 2</h1>
+    <div
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col py-10"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <p className="text-white text-4xl font-bold text-center max-w-2xl">
+          What would <span className="text-yellow-400 font-bold">YOU</span> do in <span className="text-yellow-400 font-bold">30 seconds</span>? 
+        </p>
+        <p className="text-white text-4xl font-bold text-center max-w-2xl mt-5">
+          This is <span className="text-red-400 font-bold">sextortion</span>. You’re about to learn how to <span className="text-[#a3dffd] font-bold">ESCAPE</span> it
+        </p>
+      </div>
+
+      <button className='absolute bottom-10 right-10 bg-[#8B5CF6] text-white px-4 py-2 rounded-md flex items-center gap-2'>
+        Continue <FaLongArrowAltRight />
+      </button>
     </div>
-  )
+  );
 }
 
-export default Frame2
+export default Frame2;
